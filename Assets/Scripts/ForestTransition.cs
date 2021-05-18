772cafd2165af255dd3d2ba40717cd1b5abbedc1
Eduardo Soeiro <eduardosoeiro@ua.pt>
2021-05-18 13:14:19 +0100

    Cena inicial + mesa c/ personagens

---
+++ Assets/Scripts/ForestTransition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestTransition : MonoBehaviour
+{
+    public GameObject teaPartyScreen;
+
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+    }
+}
