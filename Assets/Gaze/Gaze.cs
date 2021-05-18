b75d6ecf7775d8d934e5ae1ea7af4ad84600861d
Filipa <anafilipa.ferreira99@ua.pt>
2021-05-18 17:42:49 +0100

    Rotate globus

--- Assets/Gaze/Gaze.cs
+++ Assets/Gaze/Gaze.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Gaze : MonoBehaviour
 {
     Camera cam;
-    public float distance = 10.0f; 
+
+    public float distance = 10.0f;
+
     public GameObject pointer;
+
     Vector3 pointerScale;
 
     //Chave
@@ -18,11 +21,14 @@ public class Gaze : MonoBehaviour
 
     //Time
     float currentGazeTime = 0;
+
     public float waitingTime = 3.0f;
 
     //Animações
     Animator anim;
 
+    //Globus
+    public GameObject Globus;
 
     // Start is called before the first frame update
     void Start()
@@ -37,7 +43,6 @@ public class Gaze : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         pointer.GetComponent<Image>().enabled = cam.enabled;
 
         Ray raio = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
@@ -45,56 +50,66 @@ public class Gaze : MonoBehaviour
 
         Debug.DrawRay(raio.origin, raio.direction * distance, Color.red, 0);
 
-        if (Physics.Raycast(raio, out alvo, distance)) { 
+        if (Physics.Raycast(raio, out alvo, distance))
+        {
             print("I'm looking at " + alvo.transform.name);
-            if (alvo.transform.gameObject.CompareTag("Interactable")) {
-                pointer.transform.localScale = Vector3.Lerp(pointerScale, pointerScale * 2, 1);
+            if (alvo.transform.gameObject.CompareTag("Interactable"))
+            {
+                pointer.transform.localScale =
+                    Vector3.Lerp(pointerScale, pointerScale * 2, 1);
 
                 currentGazeTime += Time.deltaTime;
-                print("passou " + Mathf.Round(currentGazeTime) + "s"); if (currentGazeTime > waitingTime)
+                print("passou " + Mathf.Round(currentGazeTime) + "s");
+                if (currentGazeTime > waitingTime)
                 {
                     SceneManager.LoadScene(alvo.transform.gameObject.name);
                 }
             }
-            else
-
-            //Relógio
+            else //Relógio
             if (alvo.transform.gameObject.CompareTag("Clock"))
             {
-                pointer.transform.localScale = Vector3.Lerp(pointerScale, pointerScale * 2, 1);
+                pointer.transform.localScale =
+                    Vector3.Lerp(pointerScale, pointerScale * 2, 1);
 
                 print("I'm looking at " + alvo.transform.name);
-               
             }
-            else
-
-            //Porta
+            else //Porta
             if (alvo.transform.gameObject.CompareTag("Door"))
             {
-                pointer.transform.localScale = Vector3.Lerp(pointerScale, pointerScale * 2, 1);
+                pointer.transform.localScale =
+                    Vector3.Lerp(pointerScale, pointerScale * 2, 1);
 
                 print("I'm looking at " + alvo.transform.name);
 
                 anim.SetBool("doorOpen", true);
             }
-            else
-
-            //Chave
+            else //Chave
             if (alvo.transform.gameObject.CompareTag("Key"))
             {
-                pointer.transform.localScale = Vector3.Lerp(pointerScale, pointerScale * 2, 1);
+                pointer.transform.localScale =
+                    Vector3.Lerp(pointerScale, pointerScale * 2, 1);
 
                 print("I'm looking at " + alvo.transform.name);
 
                 Key.SetActive(false);
             }
 
+            //Globo
+            if (alvo.transform.gameObject.CompareTag("Globus"))
+            {
+                pointer.transform.localScale =
+                    Vector3.Lerp(pointerScale, pointerScale * 2, 1);
+
+                print("I'm looking at " + alvo.transform.name);
+                Globus.transform.Rotate(new Vector3(0, 0, 180) * Time.deltaTime);
+            }
             else
             {
                 pointer.transform.localScale = pointerScale;
             }
         }
-        else { 
+        else
+        {
             print("I'm looking at nothing!");
 
             currentGazeTime = 0;
